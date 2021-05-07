0dacfbf6d4345fbf4f1487a3f0a6f9ff8eea8dd0
Bombo43453 <81841479+Bombo43453@users.noreply.github.com>
2021-05-07 09:18:13 -0700

    dadjoke update (not don)

---
+++ commands/Fun/dadjoke.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: `dadjoke`,
+    description: `Tells A DadJoke`,
+async execute(client, message, args, Discord){
+    const jokes = [
+        'Which bear is the most condescending? A pan-duh',
+        'What kind of noise does a witch’s vehicle make? Brrrroooom, brrroooom.',
+        'What’s brown and sticky? A stick.',
+        'Two guys walked into a bar. The third guy ducked.',
+        'How do you get a country girl’s attention? A tractor.',
+        'Why are elevator jokes so classic and good? They work on many levels.',
+        'What do you call a pudgy psychic? A four-chin teller.',
+        'What did the police officer say to his belly-button? You’re under a vest',
+        'What do you call it when a group of apes starts a company? Monkey business.',
+        'My wife asked me to stop singing “Wonderwall” to her. I said maybe…',
+        'What kind of drink can be bitter and sweet? Reali-tea.',
+        'What do you call a naughty lamb dressed up like a skeleton for Halloween? Baaad to the bone.'
+    ]
+    }
+}
