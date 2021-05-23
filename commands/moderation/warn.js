febdef7fd6674f8311951ef5a2f9e82e7995df61
Bombo43453 <81841479+Bombo43453@users.noreply.github.com>
2021-05-22 17:01:03 -0700

    I believe this will fix it

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -10,7 +10,7 @@ module.exports = {
     description: `Warn A User`,
     usage: `(user) (reason)`,
 async execute(client, message, args, Discord, errorlog, botlog, msglog, profileData, guildProfile){
-    const db = require(`../../database/models/warns`)
+    const db = require(`../../database/models/Warns`)
     if(!message.member.hasPermission(`${guildProfile.WarnPerm}`)) return message.channel.send(`You Do Not Have Permission To Use This Command`)
     const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     const nousers = new Discord.MessageEmbed()
